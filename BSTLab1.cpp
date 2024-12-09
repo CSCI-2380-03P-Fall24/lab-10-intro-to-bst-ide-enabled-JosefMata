7389bfc10fbf81df629c10d5cb80733083e7d557
JosefMata <josef.mata01@utrgv.edu>
2024-12-08 19:23:44 -0600

    Finished Lab10.

--- BSTLab1.cpp
+++ BSTLab1.cpp
@@ -8,11 +8,14 @@ class Node {
 		// the Node should contain
 		// 	- an integer "key" that holds the value of the Node
 		// 	- a pointer to each child possible child node ("right" and "left")
-
+		int key;
+		Node* left;
+		Node* right;
 
 		// Write a constructor for the Node
 		// input: only an integer value for "key", no pointer arguments
 		// 		right and left pointers should be null
+		Node(int value) : key(value), left(nullptr), right(nullptr) {}
 
 };
 
@@ -20,8 +23,12 @@ class Node {
 // Use this function to create a Binary Search Tree (BST) tree with the following values
 // {13, 0, 7, 6, 21, 15, -2, 12, 99, 18, 19, -1}
 Node* createTree() {
+	int value[] = {13, 0, 7, 6, 21, 15, -2, 12, 99, 18, 19, -1}
 	// root
-	Node *root = nullptr;
+	Node* root = nullptr;
+	for (int value : values) {
+		root = insertNode(root, value);
+	}
 
 	// level 1 (children of root)
 
@@ -49,11 +56,15 @@ Node* createTree() {
 // You should write this function recursively! What is the base case? What is the general case?
 bool searchTree(int target, Node* root) {
 	// Base cases
-	return false;
+	if (root == nullptr) return false;
+	if (root->key == target) return true;
 
 	// General case
-	return false;
-	
+	if (target < root->key) {
+		return searchTree (target, root->left);
+	} else {
+		return searchTree(target, root->right);
+	}
 }
 
 // Write a function that will return the total number of Nodes in the tree
@@ -62,18 +73,19 @@ bool searchTree(int target, Node* root) {
 // You should write this function recursively!
 int treeSize(Node* root) {
 	// base case
-	return -1;
+	if (root == nullptr) return 0;
 
 	// General case
-	return -1;
+	return 1 + treeSize(root->left) + treeSize(root->right);;
 
 }
 
 // BONUS! Write a function that will determine the height of the tree
 int treeHeight(Node* root) {
 	// base case
-	return -1;
+	if (root == nullptr) return -1;
 
 	// General case
-	return -1;
+	return 1 + max(treeHeight(root->left), treeHeight(root->right));
 }
+
